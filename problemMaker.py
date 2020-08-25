abc63d1bc5eae197040ab779f1bdf38022242eb4
antdon <a.donohue@uqconnect.edu.au>
2020-08-25 15:12:08 +1000

    created a script that makes a simple question file

--- problemMaker.py
+++ problemMaker.py
@@ -1,39 +1,11 @@
-A1 = 19
-A3 = 31
-A5 = 19
-B = 10
-Ex15 = 8
-Ex16 = 9
-Ex17 = 8
-Ex18 = 8
-Ex19 = 7
-
-
+alphabet = ['a)','b)','c)','d)','e)','f)','g)','h)','i)','j)','k)','l)','m)','n)','o)','p)','q)','r)','s)','t)','u)','v)','w)','x)','y)','z)']
 if __name__ == "__main__":
-    with open ('stat2003_questions.txt', 'w') as file:
-        for question in range(1,A1+1):
-            file.write('A1 Q'+str(question)+','+'\n')
-        for question in range(1,A3+1):
-            file.write('A3 Q'+str(question)+',' +'\n')
-        for question in range(1,A5+1):
-            file.write('A5 Q' +str(question)+',' +'\n')
-        for question in range(1,B+1):
-            file.write('B Q' + str(question)+',' +'\n')
-        for question in range(1,Ex15+1):
-            file.write('2015 Exam Q' + str(question)+',' +'\n')
-        for question in range(1,Ex16+1):
-            file.write('2016 Exam Q' + str(question)+',' +'\n')
-        for question in range(1,Ex17+1):
-            file.write('2017 Exam Q' + str(question)+',' +'\n')
-        for question in range(1,Ex18+1):
-            file.write('2018 Exam Q' + str(question)+',' +'\n')
-        for question in range(1,Ex19+1):
-            file.write('2019 Exam Q' + str(question)+',' +'\n')
-        
-        
-        
-        
-        
-        
+    newFileName = input("what would you like to name your question file? ")
+    numOfQuestions = int(input("How many questions would you like in your file? "))
+    numOfSubQuestions = int(input("How many subquestions does each question have? 1-26 "))
 
-
+    
+    with open (newFileName, 'w') as file:
+        for question in range(1,numOfQuestions):
+            for subquestion in range(1,numOfSubQuestions):
+                file.write('Q' + str(question)+ alphabet[subquestion-1]+'\n')
